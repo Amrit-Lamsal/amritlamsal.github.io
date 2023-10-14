0d12a8f80f66603e618a0dab0bc3b06e31f1cfdb
Amrit Lamsal <amritlamsal400@gmail.com>
2023-10-14 10:12:42 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -284,15 +284,15 @@ new Glide($testimonialCarousel, {
 
 // <!--Start of Tawk.to Script-->
 
-// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-// (function(){
-// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-// s1.async=true;
-// s1.src='https://embed.tawk.to/650567cc0f2b18434fd8d5fb/1haehqueh';
-// s1.charset='UTF-8';
-// s1.setAttribute('crossorigin','*');
-// s0.parentNode.insertBefore(s1,s0);
-// })();
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/650567cc0f2b18434fd8d5fb/1haehqueh';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 
 // <!--End of Tawk.to Script-->
 
